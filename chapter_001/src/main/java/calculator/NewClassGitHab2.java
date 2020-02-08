21b67dff4b65eb932814fc921c0282f881127225
Vitaly Lashchenko <v.lashchenko.1976@gmail.com>
2020-02-09 01:47:08 +0200

    Add to project new file 675757

--- chapter_001/src/main/java/calculator/NewClassGitHab2.java
+++ chapter_001/src/main/java/calculator/NewClassGitHab2.java
@@ -2,6 +2,6 @@ package calculator;
 
 public class NewClassGitHab2 {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub2");
     }
 }
