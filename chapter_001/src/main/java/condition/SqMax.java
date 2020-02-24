c3111abcf4921945878eaddc33bda4046db8503c
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-02-24 22:14:22 +0200

    4.5. Отладка программы в IDEA[#228724] - ред.2

--- chapter_001/src/main/java/condition/SqMax.java
+++ chapter_001/src/main/java/condition/SqMax.java
@@ -6,17 +6,17 @@ public class SqMax {
         if (first > second) {
             if (first > third) {
                 if (first > forth) {
-                    result = first;
+                    return result = first;
                 }
             }
         }
         if (second > third) {
             if (second > forth) {
-                result = second;
+                return result = second;
             }
         }
         if (third > forth) {
-            result = third;
+            return result = third;
         }
         return result;
     }
--- chapter_001/src/main/java/condition/SqMaxTest.java
+++ chapter_001/src/main/java/condition/SqMaxTest.java
@@ -15,7 +15,7 @@ public class SqMaxTest {
         public void testSqMax2 () {
         SqMax check = new SqMax();
         int result = check.max(6, 90, 7, 5);
-        assertThat(result, is(7));
+        assertThat(result, is(90));
     }
     @Test
         public void testSqMax3 () {
@@ -29,5 +29,23 @@ public class SqMaxTest {
         SqMax check = new SqMax();
         int result = check.max(6, 4, 7, 50);
         assertThat(result, is(50));
+    }
+    @Test
+    public void testSqMax5 () {
+        SqMax check = new SqMax();
+        int result = check.max(5, 2, 4, 3);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void testSqMax6 () {
+        SqMax check = new SqMax();
+        int result = check.max(5, 4, 3, 1);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void testSqMax7 () {
+        SqMax check = new SqMax();
+        int result = check.max(5, 4, 1, 3);
+        assertThat(result, is(5));
     }
 }
