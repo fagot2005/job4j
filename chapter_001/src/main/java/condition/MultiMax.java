42265dc45757865ce8d8e039b5e886f1b7f7287c
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-02-24 21:42:04 +0200

    4.4. Максимум из трех чисел[#228726] - ред. 2

--- chapter_001/src/main/java/condition/MultiMax.java
+++ chapter_001/src/main/java/condition/MultiMax.java
@@ -3,12 +3,8 @@ package condition;
 public class MultiMax {
     public int max(int first, int second, int third) {
         int result = first;
-        if (second>result){
-            result = second;
-        }
-        if (third>result){
-            result = third;
-        }
+        result = second>result ?  second : result;
+        result = third > result ? third : result;
         return result;
     }
 
