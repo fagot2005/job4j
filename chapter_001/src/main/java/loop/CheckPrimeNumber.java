b90aa60dc148a4f4c02aff541513383520b5cf4a
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-03-06 09:53:16 +0200

    5.5. Простое число.[#228801] - ред.2

--- chapter_001/src/main/java/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/loop/CheckPrimeNumber.java
@@ -3,17 +3,16 @@ package loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
-        if (number==1){
-            return prime=false;
-        }
-        for (int i=1; i<=number; i++) {
-            if (i != 1 && i != number) {
-                if (number % i == 0) {
-                    prime = false;
-                    break;
+        if (number!=1){
+            for (int i=1; i<=number; i++) {
+                if (i != 1 && i != number) {
+                    if (number % i == 0) {
+                        prime = false;
+                        break;
+                    }
                 }
             }
-        }
+        } else prime = false;
         return prime;
     }
 }
