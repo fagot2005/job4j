35ee09f2d2c36177f0e6ca53b63d70140545f500
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-03-15 17:42:27 +0200

    5.7. Ипотека.[#228800]

---
+++ chapter_001/src/main/java/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Mortgage {
+
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double loanBalance = amount;
+        //где amount - сумма выданная по кредиту, salary - годовой доход, percent - процентная ставка по кредиту
+        while (loanBalance>0){
+            year=year+1;
+            loanBalance=loanBalance*(1+(percent/100))-salary;
+        }
+
+        return year;
+    }
+}
