70ee136583a49fd8953e5af09f48101e91bba2f0
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-03-20 22:56:32 +0200

    6.0.2. Заполнение массива.[#228750]

--- chapter_001/src/main/java/array/ArrayDefinition.java
+++ chapter_001/src/main/java/array/ArrayDefinition.java
@@ -10,5 +10,9 @@ public class ArrayDefinition {
         names[1] = "Petrov Piter";
         names[2] = "Stepanov Stepan";
         names[3] = "Antonov Antuan";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
