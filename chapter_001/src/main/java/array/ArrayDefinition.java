297a885b09bdf4f5a904e57aeff06fdbb0484621
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-03-20 22:53:37 +0200

    6.0.2. Заполнение массива.[#228750]

--- chapter_001/src/main/java/array/ArrayDefinition.java
+++ chapter_001/src/main/java/array/ArrayDefinition.java
@@ -5,5 +5,10 @@ public class ArrayDefinition {
         short [] ages= new short[10];
         String [] surnames = new String[100500];
         float [] prices = new float[40];
+        String [] names = new String[4];
+        names[0] = "Ivanov Ivan";
+        names[1] = "Petrov Piter";
+        names[2] = "Stepanov Stepan";
+        names[3] = "Antonov Antuan";
     }
 }
