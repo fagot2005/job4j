ae23cbe12ff494a9a45a1f272b4b10300d8ce1e0
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-03-22 23:12:47 +0200

    5.8. JavaDoc. Code convention[#228806] - ред.2

---
+++ chapter_001/src/main/java/calculator/Calculator.java
@@ -0,0 +1,39 @@
+package calculator;
+/**
+  * * Package for calculate task.
+ * *
+ * * @author Vitaly Lashchenko (parsentev@yandex.ru)
+ * * @version $Id$
+ * * @since 0.1
+ * */
+
+public class Calculator {
+    public static void add(double first, double second) {
+        double result =  first + second;
+        System.out.println(first + "+" + second + " = " + result);
+    }
+
+    public static void div(double first, double second) {
+        double result =  first/second;
+        System.out.println(first + "/" + second + " = " + result);
+    }
+
+    public static void multiply(double first, double second) {
+        double result =  first * second;
+        System.out.println(first + "*" + second + " = " + result);
+    }
+
+    public static void subtract(double first, double second) {
+        double result =  first - second;
+        System.out.println(first + "-" + second + " = " + result);
+    }
+
+    public static void main(String[] args) {
+        add(1, 1);
+        div(4, 2);
+        multiply(2,1);
+        subtract(10, 5);
+
+
+    }
+}
