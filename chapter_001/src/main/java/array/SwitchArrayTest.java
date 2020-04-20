69b3abfe1c3380efd51dc717932a41b9c4349901
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-04-20 19:42:15 +0300

    6.2.2. Переставить элементы массива[#228755]

---
+++ chapter_001/src/main/java/array/SwitchArrayTest.java
@@ -0,0 +1,23 @@
+package array;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwitchArrayTest {
+
+    @Test
+    void swap() {
+        int[] input = new int[] {1, 2, 3, 4, 5, 6};
+        int source = 3;
+        int dest = 5;
+        int [] result = SwitchArray.swap(input, source, dest);
+        int [] expect = new int [] {1, 2, 3, 6, 5, 4};
+        assertThat(result, is(expect));
+
+        for (int index = 0; index < expect.length; index++) {
+            System.out.println(expect[index]);
+        }
+    }
+}
