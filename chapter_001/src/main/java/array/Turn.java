f2b3d2588d7f44b0e5b1db91132dc2131b3c738b
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-04-23 22:16:19 +0300

    6.2.3. Перевернуть массив.[#228753]

---
+++ chapter_001/src/main/java/array/Turn.java
@@ -0,0 +1,24 @@
+package array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+       int countElement = array.length;
+       int temp = 0;
+            for ( int i = 0; i < countElement/2; i++ ) {
+                temp = array[i];
+                array[i] = array[array.length-1-i];
+                array[array.length-1-i] = temp;
+            }
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+        int [] temp2 = new int [] {3, 9, 45, 66, 77, 150};
+        back(temp2);
+        for (int i=0; i<=temp2.length-1; i++){
+            System.out.println(temp2[i]);
+        }
+
+    }
+}
