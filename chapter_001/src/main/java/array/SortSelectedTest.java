2048b17e9e8b304f90b8fb3bcdb790ac9c126efc
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-03 16:27:45 +0300

    6.5.3. Сортировка выборкой.[#228740]

---
+++ chapter_001/src/main/java/array/SortSelectedTest.java
@@ -0,0 +1,30 @@
+package array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SortSelectedTest {
+
+    @Test
+    public void sort() {
+        int[] input = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void sortSecond() {
+        int[] input = new int[] {5, 3, 1, 2, 1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 1, 2, 3, 5};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void sortThird() {
+        int[] input = new int[] {1, 1, 1, 1, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 1, 1, 1, 5};
+        assertThat(result, is(expect));
+    }
+}
