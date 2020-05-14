afff4fe3dcfd3b71a982c047484db450096b8090
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-15 00:57:15 +0300

    2.1. Рефакторинг - Расстояние между точками.[#228893]

--- chapter_001/src/main/java/condition/Point.java
+++ chapter_001/src/main/java/condition/Point.java
@@ -1,17 +1,25 @@
 package condition;
 
+import static java.lang.Math.*;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double distance = Math.sqrt(Math.pow((x2 - x1), 2) - Math.pow((y2 - y1), 2));
-        return distance;
+
+    public int x;
+    public int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow((this.x - that.x), 2) + pow((this.y - that.y), 2));
     }
 
     public static void main(String[] args) {
-        int x1 = 16;
-        int x2 = 4;
-        int y1 = 15;
-        int y2 = 7;
-        double result = Point.distance(x1, y1, x2, y2);
-        System.out.println("result (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + ") = " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println("result (" + a.x + ", " + a.y + ") to (" + b.x + ", " + b.y + ") = " + dist);
     }
 }
