27e8e949553a824d1f49a65f2c3093df29f87236
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-18 14:37:00 +0300

    4. Переопределение[#228910]

---
+++ chapter_002/src/main/java/inheritance/JSONReport.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator()
+                +  "name : " + name + System.lineSeparator()
+                +  "body : " + body + System.lineSeparator()
+                + "}";
+    }
+}
