29d6c49ae316cc1797c597b0b2cdbf2f8c15a5ff
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-18 12:50:52 +0300

    1. Конструктор по умолчанию.[#228908]

--- chapter_002/src/main/java/oop/Predator.java
+++ chapter_002/src/main/java/oop/Predator.java
@@ -4,10 +4,12 @@ public class Predator extends Animal {
 
     public Predator(String name) {
         super(name);
+        System.out.println(this.getClass().getName());
     }
 
     public Predator() {
         super();
+        System.out.println(this.getClass().getName());
     }
 
     public static void main(String[] args) {
--- chapter_002/src/main/java/oop/Tiger.java
+++ chapter_002/src/main/java/oop/Tiger.java
@@ -4,10 +4,12 @@ public class Tiger extends Predator {
 
     public Tiger(String name) {
         super(name);
+        System.out.println(this.getClass().getName());
     }
 
     public Tiger() {
         super();
+        System.out.println(this.getClass().getName());
     }
 
     public static void main(String[] args) {
