dc9122905f3d98c3ddcc56c14328bc9bffa0d9b1
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-28 23:43:24 +0300

    5. Реализовать класс Tracker[396#228921]

--- chapter_002/src/main/java/tracker/Tracker.java
+++ chapter_002/src/main/java/tracker/Tracker.java
@@ -1,5 +1,33 @@
 package tracker;
 
+import java.util.Random;
+
 public class Tracker {
+
+    private final Item[] items = new Item[100];
+    private int positions = 0;
+
+    private String generateId() {
+        Random rd = new Random();
+        return String.valueOf(rd.nextLong() + System.currentTimeMillis());
+
+    }
+
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[positions++] = item;
+        return item;
+    }
+
+    public Item[] findAll() {
+        return null;
+    }
+
+    public Item[] findByName(String key) {
+        return null;
+    }
+    public Item findById(String id) {
+        return null;
+    }
 
 }
