6202ef94eb350f2ca0fb130cb52618be0403d56d
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-29 18:09:27 +0300

    0.2. Кидаем исключение - throw new RuntimeException[219365#228884]

---
+++ chapter_002/src/main/java/exception/Fact.java
@@ -0,0 +1,18 @@
+package exception;
+
+public class Fact {
+    public static void main(String[] args) {
+
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Прерывается выполнение");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
