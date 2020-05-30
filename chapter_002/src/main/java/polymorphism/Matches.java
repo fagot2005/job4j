759475b962b7480ece4b0479faa330937d807f17
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-30 13:47:42 +0300

    2. Scanner и чтение числа из консоли.[179518#228866]

--- chapter_002/src/main/java/polymorphism/Matches.java
+++ chapter_002/src/main/java/polymorphism/Matches.java
@@ -6,33 +6,28 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int allCount = 11;
-        int win = 0;
+        int win = 1;
         System.out.println("Приветствую тебя в игре 11. Правила игра очень просты. ");
         System.out.println("Ты всегда начинаешь и можешь взять от 1 до 3 спичек включительно.");
         System.out.println("Проиграет тот, кто зберет последнюю спичку.");
         while (allCount > 0) {
-            System.out.println("Ход первого игрока, сколько спичек ты берешь?");
+            if (win % 2 != 0) {
+                System.out.println("Ход первого игрока, сколько спичек ты берешь?");
+            } else {
+                System.out.println("Ход второго игрока, сколько спичек ты берешь?");
+            }
             int count = Integer.valueOf(input.nextLine());
             allCount = allCount - count;
             System.out.println("Осталось " + allCount + " спичек");
             if (allCount <= 0) {
-                win = 1;
                 break;
             }
-            System.out.println();
-            System.out.println("Ход вторго игрока, сколько спичек ты берешь?");
-            int count2 = Integer.valueOf(input.nextLine());
-            allCount = allCount - count2;
-            if (allCount <= 0) {
-                win = 2;
-                break;
-            }
-            System.out.println("Осталось " + allCount + " спичек");
+            win = win + 1;
         }
-        if (win == 1) {
-            System.out.println("Cпички кончились, второй игрок выиграл");
-        } else {
+        if (win % 2 == 0) {
             System.out.println("Cпички кончились, первый игрок выиграл");
+        } else {
+            System.out.println("Cпички кончились,  второй игрок выиграл");
         }
     }
 }
