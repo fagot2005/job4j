174f20e26eec4938563eaa84cdbc46731e2545b2
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-31 19:25:40 +0300

    0.4. Пользовательские исключения.[219367#228886]

---
+++ chapter_002/src/main/java/exception/ElementNotFoundException.java
@@ -0,0 +1,28 @@
+package exception;
+
+
+
+public class ElementNotFoundException extends Exception {
+
+    public UserInputException(String message) {
+        super(message);
+    }
+
+    public static int indexOf(String[] value, String key)   {
+        int rls = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value.length == 0 || value == null) {
+                rls = index;
+            }
+            }
+        }
+
+    public static void main (String[] args) throws UserInputException {
+        String[] str = {"Eva, Mark, Eric, Marta, Denis"};
+        try {
+            if (indexOf(str, "Nic") == 0);
+        } catch (UserInputException e) {
+            e.printStackTrace();
+        }
+    }
+}
