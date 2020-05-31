dd47b79e278c2163e3f35477a938a5281900d532
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-05-31 10:26:13 +0300

    3. Удаление моделей из массива.[176470#228918]

--- chapter_002/src/main/java/pojo/Shop.java
+++ chapter_002/src/main/java/pojo/Shop.java
@@ -4,8 +4,8 @@ public class Shop {
     public static void main(String[] args) {
         Product products [] = new Product[5];
         products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
-        products[2] = new Product("Egg", 19);
+        products[3] = new Product("Bread", 4);
+        products[4] = new Product("Egg", 19);
 
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
@@ -49,8 +49,9 @@ public class Shop {
         }
 
         System.out.println();
-        System.out.println("Удаляем элемент с индекосм 1");
-        delete(products, 1);
+        System.out.println("Удаляем элемент с индекосм ");
+        //delete(products, 3);
+        delete(products, 3);
     }
 
     public static Product[] delete(Product[] products, int index) {
@@ -64,7 +65,11 @@ public class Shop {
                 System.out.println("null");
             }
         }
-        products[index] = null;
+        for (int i = index; i < products.length - 1; i++) {
+                products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+
         System.out.println();
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
