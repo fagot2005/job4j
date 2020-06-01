86ecb5fc0961017a41ff453de81a1a15f9d066ae
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-01 10:08:23 +0300

    3. Удаление моделей из массива.[176470#228918]

--- chapter_002/src/main/java/pojo/Shop.java
+++ chapter_002/src/main/java/pojo/Shop.java
@@ -6,41 +6,10 @@ public class Shop {
         products[0] = new Product("Milk", 10);
         products[3] = new Product("Bread", 4);
         products[4] = new Product("Egg", 19);
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i]; //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
-        System.out.println();
-        System.out.println("Удаляем значение из ячейки с индексом 1"); //удаляем значение из ячейки с индексом 1
+        printArray(products);
         products[1] = null;
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i]; //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
-        System.out.println();
-        System.out.println("Записываем  в ячейку с индексом "
-                + "1 значение ячейки с индексом 2 и удаляем "
-                + "значение из ячейки с индексом 2"); //записываем в ячейку с индексом 1 значение ячейки с индексом 2.
-        products[1] = products[2]; //удаляем значение из ячейки с индексом 2.
+        products[1] = products[2];
         products[2] = null;
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];   //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
-        System.out.println();
-        System.out.println("Удаляем элемент с индексом 3");
         delete(products, 3);
     }
 
@@ -50,12 +19,12 @@ public class Shop {
             products[i] = products[i + 1];
         }
         products[products.length - 1] = null;
-        System.out.println();
         printArray(products);
         return products;
     }
 
     public static void printArray(Product[] products) {
+        System.out.println();
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product != null) {
