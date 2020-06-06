695e8eaec8ae5573bc7577ada0128c2c2bafec55
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-06 19:12:05 +0300

    0.4. Пользовательские исключения.[219367#228886]

--- chapter_002/src/main/java/exception/ElementNotFoundException.java
+++ chapter_002/src/main/java/exception/ElementNotFoundException.java
@@ -2,15 +2,9 @@ package exception;
 
 public class ElementNotFoundException extends Exception {
 
-    public static void main(String[] args) throws ElementNotFoundException{
-        String[] str = {"Eva", "Mark", "Eric", "Marta", "Denis"};
-        String key = "Bil";
-        try {
-            if (FindEl.indexOf(str, key) != -1) {
-            }
-        } catch (ElementNotFoundException elementNotFoundException) {
-            elementNotFoundException.printStackTrace();
-        }
+        public ElementNotFoundException(String message) {
+        super(message);
     }
+
 }
 
--- chapter_002/src/main/java/exception/FindEl.java
+++ chapter_002/src/main/java/exception/FindEl.java
@@ -3,12 +3,23 @@ package exception;
 public class FindEl  {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (String x:value
-        ) {
-            if (x.contains(key)) {
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].contains(key)) {
                 rsl = 1;
             }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException{
+        String[] str = {"Eva", "Mark", "Eric", "Marta", "Denis"};
+        String key = "Bil";
+        try {
+            if (FindEl.indexOf(str, key) == -1) {
+                throw new ElementNotFoundException("Ошибка, елемент не найден");
+            }
+        } catch (ElementNotFoundException elementNotFoundException) {
+            elementNotFoundException.printStackTrace();
+        }
     }
 }
