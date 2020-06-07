be2e5858dc5ddae4f658c064edcad18c3902064f
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-07 13:51:39 +0300

    0.5. Иерархия исключений и множественный catch.[219368#228887]

--- chapter_002/src/main/java/exception/UserStore.java
+++ chapter_002/src/main/java/exception/UserStore.java
@@ -21,10 +21,15 @@ public class UserStore {
         User[] users = {
                 new User("Petr Arsentev", true)
         };
-        User user = findUser(users, "Petr Arsentev");
-        if (validate(user)) {
-            System.out.println("This user has an access");
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException e) {
+            e.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
         }
     }
-
 }
