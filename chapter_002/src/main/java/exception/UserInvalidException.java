6cebd76ac22ff01e254663800e3f686f1c7ca493
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-07 13:42:05 +0300

    0.5. Иерархия исключений и множественный catch.[219368#228887]

---
+++ chapter_002/src/main/java/exception/UserInvalidException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class UserInvalidException extends UserNotFoundException {
+
+    public UserInvalidException(String message) {
+        super(message);
+    }
+}
---
+++ chapter_002/src/main/java/exception/UserStore.java
@@ -0,0 +1,30 @@
+package exception;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User corectUser = new User("", false);
+        for (int i = 0; i<users.length; i++){
+            if (!users[i].getUserName().equals(login)) {
+                throw new UserNotFoundException("User is not found in Database");
+            }else corectUser = users[i];
+        }return corectUser;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.toString().length()<3){
+            throw new UserInvalidException("User is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws UserNotFoundException, UserInvalidException {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        User user = findUser(users, "Petr Arsentev");
+        if (validate(user)) {
+            System.out.println("This user has an access");
+        }
+    }
+
+}
