b977391f94b73afcdffbbbd096694a6b974e194e
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-08 18:44:36 +0300

    Конвертация листа массивов в один лист Integer[10037#228835]

---
+++ chapter_002/src/main/java/collection/ConvertList.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+    public static List<Integer> convert(List<int[]> list) {
+        List<Integer> rsl = new ArrayList<>();
+        int count = 0;
+        for (int[] x : list) {
+            for (int y: x
+            ) {
+                rsl.add(count, y);
+                count++;
+            }
+        }
+        return rsl;
+    }
+}
--- chapter_002/src/test/java/collection/ConvertList2ArrayTest.java
+++ chapter_002/src/test/java/collection/ConvertList2ArrayTest.java
@@ -3,7 +3,10 @@ package collection;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -22,5 +25,14 @@ public class ConvertList2ArrayTest {
                 {7, 0, 0}
         };
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTwoList() {
+        List<int[]> in = new ArrayList<>();
+        in.add(new int[] {1});
+        in.add(new int[] {2, 3});
+        List<Integer> expect = Arrays.asList(1, 2, 3);
+        assertThat(ConvertList.convert(in), is(expect));
     }
 }
