cfc89b34fc8dd8d070c5a6bea783d1acfaee8463
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-08 13:15:00 +0300

    2. Очередь с приоритетом на LinkedList[41670#228832]

--- chapter_002/src/main/java/collection/PriorityQueue.java
+++ chapter_002/src/main/java/collection/PriorityQueue.java
@@ -8,18 +8,14 @@ public class PriorityQueue {
 
     public void put(Task task) {
         int index = 0;
-        for (int i = 0; i < tasks.size(); i++) {
-            //System.out.println(tasks);
-            if ( tasks.get(i).getPriority() == this.tasks.get(i).getPriority() ) {
-                index = i;
-                //System.out.println(tasks.get(i).getDesc() + "  " + tasks.get(i).getPriority());
-            }
+        int counter = 0;
+        for (Task x: tasks) {
+            if ( x.getPriority() == this.tasks.get(counter).getPriority()) {
+                index = counter;
+        }
+            counter ++;
         }
         this.tasks.add(index, task);
-//        System.out.println();
-//        for (int i = 0; i < tasks.size(); i++) {
-//            System.out.println(tasks.get(i).getDesc() + tasks.get(i).getPriority());
-//        }
     }
 
     public Task take() {
--- chapter_002/src/test/java/collection/PriorityQueueTest.java
+++ chapter_002/src/test/java/collection/PriorityQueueTest.java
@@ -1,14 +1,8 @@
 package collection;
 
 import org.junit.Test;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
-import static org.junit.Assert.*;
 
 public class PriorityQueueTest {
 
