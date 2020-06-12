1403aa3f35a0c7c5d683681ef2e6a8b714b9a73f
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-13 00:15:48 +0300

    3. Банковские переводы. [#228841]

---
+++ chapter_002/src/test/java/banksystem/BankServiceTest.java
@@ -0,0 +1,16 @@
+package banksystem;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class BankServiceTest {
+    @Test
+    public void addUser() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        assertThat(bank.findByPassport("3434"), is(user));
+    }
+}
