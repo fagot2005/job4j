38825e4bbe39c77d88054322c8de3516fb65568b
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-16 17:11:35 +0300

    3. Компаратор для строк. [#228825]

--- chapter_002/src/main/java/sort/StringCompare.java
+++ chapter_002/src/main/java/sort/StringCompare.java
@@ -6,27 +6,31 @@ public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        int resalt = 0;
-        if (o1.length() > o2.length()) {
-            return 1;
-        } else if (o1.length() < o2.length()) {
-            return -1;
-        } else {
-            for (int i = 0; i < Math.max(o1.length(), o2.length()); i++) {
-                if (o1.charAt(i) == o2.charAt(i)) {
-                    resalt = 0;
-                } else if (o1.charAt(i) > o2.charAt(i)) {
-                    resalt = 1;
-                    break;
-                } else {
-                    resalt = -1;
-                    break;
-                }
-            } return resalt;
+        int resalt = -1;
+        //int iteration = Math.min(o1.length(), o2.length());
+        for (int i = 0; i < Math.min(o1.length(), o2.length()); i++) {
+            if (Character.compare(o1.charAt(i), o2.charAt(i)) == 0) {
+                resalt = 0;
+            } else break;
         }
-//
-//        if (o1.length() == o2.length()) {
-//
-//        } else
+        if (resalt == 0 && Integer.compare(o1.length(), o2.length()) == 0) {
+            resalt = 0;
+        } else if (resalt == 0 && Integer.compare(o1.length(), o2.length()) > 0) {
+            resalt = 1;
+        } else resalt = -1;
+//            else if (o1.charAt(i) > o2.charAt(i)) {
+//                resalt = 1;
+//                break;
+//            } else {
+//                resalt = -1;
+//                break;
+//            }
+//            int resalt = 0;
+//            if (o1.length() > o2.length()) {
+//                return 1;
+//            } else if (o1.length() < o2.length()) {
+//                return -1;
+//            } else {
+        return resalt;
     }
 }
