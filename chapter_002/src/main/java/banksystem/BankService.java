4de46da088e529f29913d5581846802b56196e4b
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-16 11:20:47 +0300

    3. Банковские переводы.[10038#228841]

--- chapter_002/src/main/java/banksystem/BankService.java
+++ chapter_002/src/main/java/banksystem/BankService.java
@@ -9,20 +9,55 @@ public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        users.putIfAbsent(user, new ArrayList());
+        users.put(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
-
+        //System.out.println(account);
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(account);
+        users.put(findByPassport(passport), accounts);
     }
 
     public User findByPassport(String passport) {
+        for (Map.Entry<User, List<Account>>  entry: users.entrySet()
+        ) {
+            if (entry.getKey().passport == passport) {
+                return entry.getKey();
+            }
+        }
         return null;
     }
 
     public Account findByRequisite(String passport, String requisite) {
+        for (Map.Entry<User, List<Account>>  entry: users.entrySet()
+             ) {
+//            for (List acauntOfUsers: users.values()
+//                 ) {
+            for (Map entry.getValue(). acc:
+                 ) {
+
+            }
+                System.out.println(entry.getKey() + ", " + entry.getValue());
+//                if (entry.getKey().passport = passport &&
+//                        acauntOfUsers.set(acauntOfUsers., users.values()) == requisite) {
+//
+//            }
+
+        }
+//        List<Account> allAcauntOfUsers = new ArrayList<>();
+////        for (Map.Entry <User, List<Account>> entry: users.entrySet()
+////             ) {
+//            for (List<Account> : accounts
+//                 ) {
+//                User user = entry.getKey();
+////                if (user.passport == passport &&
+//                        allAcauntOfUsers.set(user.passport, ) == requisite)
+            //}
         return null;
-    }
+        }
+
+
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String dеstRequisite, double amount) {
--- chapter_002/src/test/java/banksystem/BankServiceTest.java
+++ chapter_002/src/test/java/banksystem/BankServiceTest.java
@@ -13,4 +13,13 @@ public class BankServiceTest {
         bank.addUser(user);
         assertThat(bank.findByPassport("3434"), is(user));
     }
+
+    @Test
+    public void addAccount() {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
+    }
 }
