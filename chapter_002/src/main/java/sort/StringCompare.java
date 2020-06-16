5807be617cedf117a7bf62516cacc02158ebbb08
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-16 22:08:40 +0300

    3. Компаратор для строк. [#228825]

--- chapter_002/src/main/java/sort/StringCompare.java
+++ chapter_002/src/main/java/sort/StringCompare.java
@@ -9,22 +9,12 @@ public class StringCompare implements Comparator<String> {
         int resalt = -1;
         int strLenght = Math.min(o1.length(), o2.length());
         for (int i = 0; i < strLenght; i++) {
-            int intComp = Integer.compare(o1.length(), o2.length());
             int resComp = Character.compare(o1.charAt(i), o2.charAt(i));
             if (resComp != 0) {
                 break;
             }
-            return resalt == 0 ? intComp : resComp;
+            return resalt == 0 ? Integer.compare(o1.length(), o2.length()) : resComp;
         }
-//
-//
-//        if (resalt == 0 && Integer.compare(o1.length(), o2.length()) == 0) {
-//            resalt = 0;
-//        } else if (resalt == 0 && Integer.compare(o1.length(), o2.length()) > 0) {
-//            resalt = 1;
-//        } else resalt = -1;
-////
-//
-        return -1;
+        return resalt;
     }
 }
