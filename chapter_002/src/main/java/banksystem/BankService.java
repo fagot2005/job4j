17309798a6206575784802adba4f38d61f30a421
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-18 17:29:57 +0300

    3. Банковские переводы. [#228841]

--- chapter_002/src/main/java/banksystem/BankService.java
+++ chapter_002/src/main/java/banksystem/BankService.java
@@ -17,7 +17,6 @@ public class BankService {
         if (user != null) {
             List<Account> accounts = users.get(user);
             accounts.add(account);
-            users.put(findByPassport(passport), accounts);
         }
     }
 
@@ -46,16 +45,11 @@ public class BankService {
         return null;
     }
 
-
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String dеstRequisite, double amount) {
-        User user = findByPassport(srcPassport);
-        //System.out.println(user.getUsername());
-        Account accountMinus = findByRequisite(user.getPassport(), srcRequisite);
-        Account accountPlus = findByRequisite(user.getPassport(), dеstRequisite);
-        //System.out.println(account.requisite);
-        //BankService.findByRequisite
-        if (accountMinus.getBalance() >= amount) {
+        Account accountMinus = findByRequisite(srcPassport, srcRequisite);
+        Account accountPlus = findByRequisite(destPassport, dеstRequisite);
+        if (accountMinus.getBalance() >= amount && (accountMinus != null || accountPlus != null)) {
             accountMinus.setBalance(accountMinus.getBalance() - amount);
             accountPlus.setBalance(accountPlus.getBalance() + amount);
             //System.out.println(account.getBalance());
@@ -63,13 +57,4 @@ public class BankService {
         boolean rsl = false;
         return rsl;
     }
-
-//    public static void main(String[] args) {
-//        List<Account> accounts = new ArrayList<>();
-//        String requisite = "3fdsbb9";
-//        accounts.add(new Account("3fdsbb9", 140));
-//        int index = accounts.indexOf(new Account(requisite, -1));
-//        Account find = accounts.get(index);
-//        System.out.println(find.getRequisite() + " -> " + find.getBalance());
-//    }
 }
