a2f3f16c675e1bf0454d2f24bdfd6458c0df2cfc
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-06-21 22:02:16 +0300

    6. В классе StartUI заменить вывод в консоль на Consumer. [#228689]

--- chapter_002/src/main/java/collection/PhoneDicsionary.java
+++ chapter_002/src/main/java/collection/PhoneDicsionary.java
@@ -1,20 +1,36 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDicsionary {
-    private ArrayList<Person> person = new ArrayList<Person>();
+    private static ArrayList<Person> persons = new ArrayList<Person>();
 
     public void add(Person person) {
-        this.person.add(person);
+        this.persons.add(person);
     }
 
     public ArrayList<Person> found(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        System.out.println(person.get(0).getSurname());
-        for (int i = 0; i < person.size(); i++) {
-            if (person.get(i).getName().contains(key) || person.get(i).getSurname().contains(key) || person.get(i).getAddress().contains(key) || person.get(i).getPhone().contains(key)) {
-                result.add(person.get(i));
+        System.out.println(persons.get(0).getSurname());
+        for (int i = 0; i < persons.size(); i++) {
+            if (persons.get(i).getName().contains(key) || persons.get(i).getSurname().contains(key) || persons.get(i).getAddress().contains(key) || persons.get(i).getPhone().contains(key)) {
+                result.add(persons.get(i));
+            }
+        }
+        return result;
+    }
+
+    public static ArrayList<Person> find(String key) {
+        Predicate<Person> combineAddress = x -> x.getAddress().contains(key);
+        Predicate<Person> combineSurname = x -> x.getSurname().contains(key);
+        Predicate<Person> combinePhone = x -> x.getAddress().contains(key);
+        Predicate<Person> combineName = x -> x.getName().contains(key);
+        Predicate<Person> or = combineAddress.or(combineSurname).or(combinePhone).or(combineName);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (or.test(person)) {
+                result.add(person);
             }
         }
         return result;
--- chapter_002/src/test/java/collection/PhoneDicsionaryTest.java
+++ chapter_002/src/test/java/collection/PhoneDicsionaryTest.java
@@ -1,13 +1,9 @@
 package collection;
 
 import org.junit.Test;
-
 import java.util.ArrayList;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
-import static org.junit.Assert.*;
 
 public class PhoneDicsionaryTest {
 
@@ -22,13 +18,21 @@ public class PhoneDicsionaryTest {
         phones.add(new Person("Seter", "Ivanov", "534872", "Bryansk"));
         phones.add(new Person("Vasil", "Retearov", "534872", "Bretnsk"));
         phones.add(new Person("1", "2", "534872", "Bretnsk"));
-        //phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        //phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
         ArrayList<Person> persons = phones.found("et");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
         assertThat(persons.get(1).getSurname(), is("Ivanov"));
         assertThat(persons.get(2).getSurname(), is("Retearov"));
         assertThat(persons.get(3).getSurname(), is("2"));
+    }
+
+    @Test
+    public void whenFindByName() {
+        PhoneDicsionary phones = new PhoneDicsionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = PhoneDicsionary.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
 }
