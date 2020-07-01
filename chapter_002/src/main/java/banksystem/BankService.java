fa16d49f39bc4fae2b7ed7fff48cc2b47ee88fae
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-01 23:06:36 +0300

    3. Optional в банковских переводах. [#228706]

--- chapter_002/src/main/java/banksystem/BankService.java
+++ chapter_002/src/main/java/banksystem/BankService.java
@@ -30,21 +30,22 @@ public class BankService {
         Account account = null;
         Optional<User> user = findByPassport(passport);
         if (!user.isEmpty()) {
-            List<Account> accounts = users.get(user);
+            List<Account> accounts = users.get(user.get());
             return accounts.stream()
                     .filter(account1 -> account1.getRequisite().equals(requisite))
                     .findFirst();
         }
-        return null;
+        return Optional.empty();
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String dеstRequisite, double amount) {
         Optional<Account> accountMinus = findByRequisite(srcPassport, srcRequisite);
         Optional<Account> accountPlus = findByRequisite(destPassport, dеstRequisite);
-        if ((!accountMinus.isEmpty() && !accountPlus.isEmpty()) && accountMinus.getBalance() >= amount) {
-            accountMinus.setBalance(accountMinus.getBalance() - amount);
-            accountPlus.setBalance(accountPlus.getBalance() + amount);
+        if ((!accountMinus.isEmpty() && !accountPlus.isEmpty())
+                && accountMinus.get().getBalance() >= amount) {
+            accountMinus.get().setBalance(accountMinus.get().getBalance() - amount);
+            accountPlus.get().setBalance(accountPlus.get().getBalance() + amount);
             //System.out.println(account.getBalance());
         }
         boolean rsl = false;
