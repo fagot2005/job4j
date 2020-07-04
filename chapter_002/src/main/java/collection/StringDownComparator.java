8ff1fe1f992f68dba21c4d53d47bbf49e3c209d5
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-07-04 11:26:43 +0300

    Отсортировать департаменты [#228822]

--- chapter_002/src/main/java/collection/StringDownComparator.java
+++ chapter_002/src/main/java/collection/StringDownComparator.java
@@ -6,19 +6,9 @@ public class StringDownComparator implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        String o1BeforeSlash;
-        String o2BeforeSlash;
-        if (o1.contains("/")) {
-            o1BeforeSlash = o1.substring(0, o1.indexOf("/"));
-        } else {
-            o1BeforeSlash = o1;
-        }
-        if (o2.contains("/")) {
-            o2BeforeSlash = o2.substring(0, o2.indexOf("/"));
-        } else {
-            o2BeforeSlash = o2;
-        }
-        int result = o2BeforeSlash.compareTo(o1BeforeSlash);
+        String[] splitO1 = o1.split("/");
+        String[] splitO2 = o2.split("/");
+        int result = splitO2[0].compareTo(splitO1[0]);
         return result == 0 ? o1.compareTo(o2) : result;
     }
 }
